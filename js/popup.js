ed242456cbbb0431d87bd5f405b34c302c158fb1
Toby <islanddd@icloud.com>
2018-05-10 01:26:35 +0800

    feat: :triangular_flag_on_post:  Filter List

--- js/popup.js
+++ js/popup.js
@@ -1,26 +1,26 @@
 console.log('Popup.js is running!');
 
 $(function () {
+
     chrome.management.getAll(function (info) {
-        
         /* Set Search Box Placeholder */
         $('#myInput').attr('placeholder', `Search ${info.length} extensions`);
-       
+
         /* Operate DOM and Append Elements Dynamically */
         info.forEach(element => {
-            
+
             /* Get Each Extension's Info */
             let shortName = element.shortName;
             let icons = "/assets/images/null.jpg";
-            if(typeof(element.icons) !== "undefined") {
+            if (typeof (element.icons) !== "undefined") {
                 icons = element.icons[0].url;
             }
-            let enabled = element.enabled ? "checked":"";
+            let enabled = element.enabled ? "checked" : "";
             let description = element.description;
             let homepageUrl = element.homepageUrl;
 
             $('.container')
-                .append(`<div class="extension" >          
+                .append(`<div class="extension">          
                             <input type="checkbox" ${enabled}>
                             <img class="icons" id="${shortName}_icon" src=${icons}>
                             <span class="shortName_${enabled} mySpan" href="${homepageUrl}">${shortName}</span>  
@@ -29,16 +29,32 @@ $(function () {
         console.log(info);
     });
 
-    /* Click and Open the Home Page */
-    $('.container').on('click', 'span', function (param) {  
+    /* Events */
+
+    /* Click Event */
+    $('.container').on('click', 'span', function (param) {
         var url = $(this).attr('href');
         window.open(url);
     });
 
-    // $('#myInput').keyup(searchExtensions());
-    
-    /* Search Extension */
+    /* Keyup Event */
+    $('#myInput').keyup(searchExtensions);
+
+    /* Search Extension  */
     function searchExtensions() {
-        console.log($('#myInput'));
+        var input = $(this).val().toUpperCase();
+        var cnt = $('.container');
+        var extensions = $('div.extension');
+        var shortName = "";
+
+        extensions.each(function (index, element) {
+            shortName = $(element).find('span').html();
+
+            if (shortName.toUpperCase().indexOf(input) > -1) {
+                $(element).css('display', "block");
+            } else {
+                $(element).css('display', "none");
+            }
+        });
     }
 });
