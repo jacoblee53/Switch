8699ac71d52d06f760c7243464c390dea1a4d982
Toby <islanddd@icloud.com>
2018-05-10 00:18:32 +0800

    feat: Click shortname and open tht homepage

--- js/popup.js
+++ js/popup.js
@@ -1,33 +1,44 @@
-$(function () {
-    console.log('Popup.js is running!');
+console.log('Popup.js is running!');
 
+$(function () {
     chrome.management.getAll(function (info) {
         
         /* Set Search Box Placeholder */
         $('#myInput').attr('placeholder', `Search ${info.length} extensions`);
        
-        /* Operate DOM */
+        /* Operate DOM and Append Elements Dynamically */
         info.forEach(element => {
             
             /* Get Each Extension's Info */
             let shortName = element.shortName;
-            let icons = null;
-            if(typeof(element["icons"]) !== "undefined") {
+            let icons = "/assets/images/null.jpg";
+            if(typeof(element.icons) !== "undefined") {
                 icons = element.icons[0].url;
             }
-
             let enabled = element.enabled ? "checked":"";
             let description = element.description;
             let homepageUrl = element.homepageUrl;
 
             $('.container')
-                .append(`<div class="extension">          
+                .append(`<div class="extension" >          
                             <input type="checkbox" ${enabled}>
                             <img class="icons" id="${shortName}_icon" src=${icons}>
-                            <span class="shortName_${enabled} mySpan"> ${shortName}</span>  
+                            <span class="shortName_${enabled} mySpan" href="${homepageUrl}">${shortName}</span>  
                         </div>`);
         });
         console.log(info);
     });
 
+    /* Click and Open the Home Page */
+    $('.container').on('click', 'span', function (param) {  
+        var url = $(this).attr('href');
+        window.open(url);
+    });
+
+    // $('#myInput').keyup(searchExtensions());
+    
+    /* Search Extension */
+    function searchExtensions() {
+        console.log($('#myInput'));
+    }
 });
